4d1ce3e39afbe5cd90737d5a066c1a22fbd00518
Kavya Jinna <jinnakavya@gmail.com>
2015-04-11 23:22:19 -0400

    Blood Bank Src
    Kavya Jinna

---
+++ Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using Blood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blood.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        [HttpGet]
+        [ActionName("Register")]
+        public ActionResult Register_Get()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Register")]
+        public ActionResult Register_Post(user u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (BloodDatabaseEntities dc = new BloodDatabaseEntities())
+                {
+                    //you should check duplicate registration here 
+                    dc.users.Add(u);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+
+                    u = null;
+                    ViewBag.Message = "Successfully Registration Done";
+                }
+            }
+            return View(u);
+        }
+
+        [HttpGet]
+        [ActionName("Search")]
+        public ActionResult Search_Get()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        [ActionName("Search")]
+        public ActionResult Search_Post(string State, string blood_type)
+        {
+            List<user> items;
+            BloodDatabaseEntities db = new BloodDatabaseEntities();
+
+            if (State != "Choose a state" && blood_type != "Choose blood type")
+            {
+            items = db.users.Where(x=>x.State == State && x.BloodType == blood_type).ToList();
+            }
+            else if (State == "Choose a state" && blood_type != "Choose blood type")
+            {
+            items = db.users.Where(x=>x.BloodType == blood_type).ToList();
+            }
+            else if (State != "Choose a state" && blood_type == "Choose blood type")
+            {
+            items = db.users.Where(x => x.State == State).ToList();
+            }
+            else
+            {
+            items = db.users.ToList();
+            }
+
+
+            if(items.Count != 0)
+            {
+            ViewBag.Summary = "";
+            return View("Result",items);
+            }
+            else
+            {
+                ViewBag.Summary = "No Results Found";
+                return View("Result", items);
+            }
+        }
+
+
+    }
+}
---
+++ Models/User1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blood.Models
+{
+    [MetadataType(typeof(usermetadata))]
+    public partial class user
+    {
+
+    }
+
+    public class usermetadata
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please provide First name", AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please provide Last Name", AllowEmptyStrings = false)]
+        public string LastName { get; set; }
+         [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
+                    ErrorMessage = "Please provide valid email id")]
+         [Required(ErrorMessage = "Please provide Email Id", AllowEmptyStrings = false)]
+        public string EmailId { get; set; }
+         [Required(ErrorMessage = "Please provide Address", AllowEmptyStrings = false)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please provide state", AllowEmptyStrings = false)]
+        public string State { get; set; }
+        [Required(ErrorMessage = "Please provide Zipcode", AllowEmptyStrings = false)]
+        public string Zipcode { get; set; }
+        [Required(ErrorMessage = "Please provide Blood Type", AllowEmptyStrings = false)]
+        public string BloodType { get; set; }
+    }
+}
